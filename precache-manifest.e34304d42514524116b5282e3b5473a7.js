73aa3f598d9e4cb221bdbf5ae6b5eb33cd0450fc
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-04-30 01:47:12 +0530

    Updates

---
+++ precache-manifest.e34304d42514524116b5282e3b5473a7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd6e7d137023d1033e19d850141f31d",
+    "url": "/GoogleKeepClone/index.html"
+  },
+  {
+    "revision": "0f459814e8ae4dcd4bb5",
+    "url": "/GoogleKeepClone/static/js/2.b09c5e27.chunk.js"
+  },
+  {
+    "revision": "3d0a8d480a4d30e3bd47",
+    "url": "/GoogleKeepClone/static/js/main.0ee19be1.chunk.js"
+  },
+  {
+    "revision": "182714b51e47535869e5",
+    "url": "/GoogleKeepClone/static/js/runtime-main.06b32128.js"
+  }
+]);
