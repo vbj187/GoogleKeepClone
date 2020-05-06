c4dc5ba77f3054ba9eb3ddcb3935529986accd0b
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-06 16:00:03 +0530

    minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
 function Footer() {
-    const year = new Date().getFullYear();
     return (
         <footer>
-            <p>Copyright ⓒ {year}</p>
+            <p>Made by Vigneshwaran</p>
         </footer>
     );
 }
